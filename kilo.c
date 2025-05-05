256c7511602c77023ef9d20dfd85e42b2669a145
Laptop WSL-Ubuntu <https://github.com/ballooner/c-practice.git>
2025-05-05 14:37:13 -0400

    Entering raw mode chapter complete

--- kilo.c
+++ kilo.c
@@ -1,42 +1,72 @@
+/** includes **/
+
 #include <ctype.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
 
+/** data **/
+
 struct termios originalTerminalParameters;
+
+/** terminal **/
+
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
 
 void leaveRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTerminalParameters);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTerminalParameters) == -1)
+        die("tcsetattr");
 }
 
 void enterRawMode()
 {
-    tcgetattr(STDIN_FILENO, &originalTerminalParameters);
+    if (tcgetattr(STDIN_FILENO, &originalTerminalParameters) == -1)
+        die("tcgetattr");
+
+    
     atexit(leaveRawMode);
  
     struct termios terminalParameters = originalTerminalParameters;
-   
-    terminalParameters.c_lflag &= ~(ECHO | ICANON | ISIG);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &terminalParameters);
+    terminalParameters.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    terminalParameters.c_oflag &= ~(OPOST);
+    terminalParameters.c_cflag |= (CS8);
+    terminalParameters.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    terminalParameters.c_cc[VMIN] = 0;
+    terminalParameters.c_cc[VTIME] = 1;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &terminalParameters) == -1)
+        die("tcsetattr");
 }
+
+/** init **/
 
 int main()
 {
     enterRawMode();
 
-    char c;
-    while ((read(STDIN_FILENO, &c, 1) == 1) && c != 'q')
+    while (1)
     {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
+
         if (iscntrl(c))
         {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else 
         {
-            printf("%c: %d\n", c, c);
+            printf("%c: %d\r\n", c, c);
         }
+
+        if (c == 'q') break;
     }
 
     return 0;
